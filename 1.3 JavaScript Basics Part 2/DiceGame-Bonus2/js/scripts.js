387e9a14cd827b2665bf87c1e2b457af62354912
sammy khaleel <sammykhaleel83@gmail.com>
2019-06-27 02:24:04 -0400

    complete JavaScript Basics Part 2 tasks

---
+++ 1.3 JavaScript Basics Part 2/DiceGame-Bonus2/js/scripts.js
@@ -0,0 +1,21 @@
+//Generate the pc random number
+var pcRandomNumber = Math.random() * 9 + 1;
+var pcDiceRoll = Math.round(pcRandomNumber);
+var pcResult = "The computer's result is: " + pcDiceRoll + "!";
+// Output computer results:
+document.write(pcResult + "<br>");
+
+// Generate the player's game results:
+var playerRandomNumber = Math.random() * 9 + 1;
+var playerDiceRoll = Math.round(playerRandomNumber);
+var playerResult = "Your result is: " + playerDiceRoll + "!";
+// Output player results:
+console.log(playerResult);
+document.write(playerResult + "<br>");
+
+//Determine which one is the winner.
+if (pcDiceRoll > playerDiceRoll) {
+  document.write("The computer is the winner!");
+} else if (playerDiceRoll > pcDiceRoll) {
+  document.write("You are the winner!");
+}
