259b0fb17f4bdb3c5765c252e5aeb7ba50abab40
sammy khaleel <sammykhaleel83@gmail.com>
2019-07-03 09:26:41 -0400

    Part 2 forEach Loops

--- 1.4 JavaScript Functions/Part 2 forEach Loops/js/scripts.js
+++ 1.4 JavaScript Functions/Part 2 forEach Loops/js/scripts.js
@@ -1,73 +1,45 @@
-//Task 3.Creating a pokemons array to hold pokemons data
 var repository = [
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"]
-  },
-  {
-    name: "Golbat",
-    height: 1.6,
-    types: ["poison"]
-  },
-  {
-    name: "Alakazam",
-    height: 1.5,
-    types: ["psychic"]
-  },
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "dirt"]
-  },
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: ["water"]
-  }
+  { name: "Bulbasaur", height: 0.7, types: ["grass"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
+  { name: "Alakazam", height: 1.5, types: ["psychic"] }
 ];
 
-//Bonus Task 1.Checking each pokemon type and giving a uniqe color to each type
-for (var i = 0; i < repository.length; i++) {
-  var result;
-  for (var j = 0; j < repository[i].types.length; j++) {
-    if (repository[i].types[j] == "grass") {
-      result = '<span style="color:green;"> ';
-    } else if (repository[i].types[j] == "fire") {
-      result = '<span style="color:red;"> ';
-    } else if (repository[i].types[j] == "psychic") {
-      result = '<span style="color:orange;"> ';
-    } else if (repository[i].types[j] == "poison") {
-      result = '<span style="color:rgb(106, 42, 106);"> ';
-    } else if (repository[i].types[j] == "water") {
-      result = '<span style="color:blue;"> ';
-    }
-  }
-
-  //Task 6.Checking the size of each pokemon.
+repository.forEach(function(item) {
+  // for (var item = 0; item < repository.length; item++) {
   var size;
-  if (repository[i].height > 1) {
+  if (item.height > 1) {
     size = "Wow, that’s big!";
   } else {
-    size = "Is a small";
+    size = "It's small pokemon";
   }
 
-  //Task 4.Display pokemons keys and values
+  var result;
+  item.types.forEach(function(typeItem) {
+    // for (var typeItem = 0; typeItem < repository[item].types.length; typeItem++) {
+    if (typeItem == "grass") {
+      result = '<span style="color:green;"> ';
+    } else if (typeItem == "fire") {
+      result = '<span style="color:red;"> ';
+    } else if (typeItem == "psychic") {
+      result = '<span style="color:yellow;"> ';
+    } else if (typeItem == "poison") {
+      result = '<span style="color:rgb(106, 42, 106);"> ';
+    }
+  });
   document.write(
-    '<div class = "box">' +
-      repository[i].name +
-      " " +
-      "(Height: " +
-      repository[i].height +
+    '<div class="box">' +
+      item.name +
+      "(height: " +
+      item.height +
       "m" +
       ")" +
       "<br>" +
-      repository[i].types +
-      "<br>" +
+      size +
       result +
       "<br>" +
-      size +
-      "</div>" +
-      "<br>"
+      item.types +
+      "<br>" +
+      "</div>"
   );
-}
+});
