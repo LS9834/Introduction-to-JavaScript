62b662515767b19c7a81e9b4aef5928b541767c4
sammy khaleel <sammykhaleel83@gmail.com>
2019-07-09 15:24:02 -0400

    first commit

---
+++ 1.5 Intro to Functional Programming/js/scripts.js
@@ -0,0 +1,69 @@
+var pokemonRepository = (function() {
+  var repository = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
+    { name: "Alakazam", height: 1.5, types: ["psychic"] }
+  ];
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(item) {
+  var size;
+  if (item.height > 1) {
+    size = "Wow, that’s big!";
+  } else {
+    size = "It's small pokemon";
+  }
+
+  var result;
+  item.types.forEach(function(typeItem) {
+    if (typeItem == "grass") {
+      result = '<span style="color:green;"> ';
+    } else if (typeItem == "fire") {
+      result = '<span style="color:red;"> ';
+    } else if (typeItem == "electric") {
+      result = '<span style="color:yellow;"> ';
+    } else if (typeItem == "poison") {
+      result = '<span style="color:rgb(106, 42, 106);"> ';
+    } else if (typeItem == "psychic") {
+      result = '<span style="color:orange;"> ';
+    }
+  });
+  document.write(
+    '<div class="box">' +
+      item.name +
+      "(height: " +
+      item.height +
+      "m" +
+      ")" +
+      "<br>" +
+      size +
+      result +
+      "<br>" +
+      item.types +
+      "<br>" +
+      "</div>"
+  );
+});
